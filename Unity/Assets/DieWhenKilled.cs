5935316608625e6745f5d80d16cda623ceab0f77
I-have-no-pants <jesper.tingvall@gmail.com>
2013-09-19 10:11:34 +0200

    Added health component

---
+++ Unity/Assets/DieWhenKilled.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieWhenKilled : HealthComponent {
+
+	protected override void OnDeath ()
+	{
+		Destroy(this.gameObject);
+	}
+	
+	protected override void OnDamage (int damage)
+	{}
+}
---
+++ Unity/Assets/HealthComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract  class HealthComponent : MonoBehaviour {
+	
+	public int maxHealth;
+	
+	private int health;
+	public int Health {
+		get {
+			return health;
+		}
+		private set {
+			if (value < 0)
+				OnDamage (value);
+			health =value;
+			if (health < 0)
+				OnDeath ();
+		}
+			
+	}
+	
+	
+	protected abstract void OnDeath();
+	protected abstract void OnDamage(int damage);
+}
