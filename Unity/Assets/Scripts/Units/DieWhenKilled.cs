582b99773b352e7f247df78a280f30e665527347
I-have-no-pants <jesper.tingvall@gmail.com>
2013-09-28 20:36:28 +0200

    Added particle systems on damage and death

--- Unity/Assets/Scripts/Units/DieWhenKilled.cs
+++ Unity/Assets/Scripts/Units/DieWhenKilled.cs
@@ -2,12 +2,35 @@
 using System.Collections;
 
 public class DieWhenKilled : HealthComponent {
-
+	
+	public GameObject DeathEffect;
+	public float DeathEffectLifeTime;
+	
+	public GameObject Damage;
+	public float DamageTime;
+	
+	private float damageTimer;
+	
 	protected override void OnDeath ()
 	{
+		Destroy(Instantiate(DeathEffect,transform.position,Quaternion.identity) as GameObject,DeathEffectLifeTime);
 		Destroy(this.gameObject);
 	}
 	
 	protected override void OnDamage (int damage) {
+		if (Damage) {
+			damageTimer = DamageTime;
+			Damage.particleSystem.enableEmission = true;
+		}
 	}
+	
+	void FixedUpdate () {
+		if (Damage) {
+			if (damageTimer>0)
+				damageTimer-=Time.fixedDeltaTime;
+			else if (Damage.particleSystem.enableEmission == true)
+				Damage.particleSystem.enableEmission = false;
+		}
+	}
+	
 }
